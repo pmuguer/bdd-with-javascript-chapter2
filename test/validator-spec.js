3f09cee87967b7b889be601d90172d6bbcf81c68
Pablo Muguerza <pmuguer@gmail.com>
2019-04-06 07:12:20 -0300

    Agregadas assertions usando chai

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,11 +1,10 @@
-var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
 var validator = require('../lib/validator');
 
 describe('A Validator', function() {
     it('will return error.nonpositive for not strictly positive numbers', 
         function() {
-            assert.deepEqual(validator(0), ['error.nonpositive']);
+            expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
         });
     });
-
-
