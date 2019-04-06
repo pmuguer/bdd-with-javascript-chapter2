480ecdeac4e4c96fbc850fca660d11c927c1621b
Pablo Muguerza <pmuguer@gmail.com>
2019-04-06 06:57:36 -0300

    movido el código fuera del test

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,7 +1,5 @@
 var assert = require('assert');
-
-function validator() {
-}
+var validator = require('../lib/validator');
 
 describe('A Validator', function() {
     it('will return error.nonpositive for not strictly positive numbers', 
